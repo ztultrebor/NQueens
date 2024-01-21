83279702dfc5077906d023da4c34788f1d8456b0
Robert Lutz <robertlutzi@gmail.com>
2024-01-21 16:12:34 -0500

    ensures queen is placed in least threatening square

--- 8queens.rkt
+++ 8queens.rkt
@@ -13,7 +13,8 @@
   ; attempts to solve the NQueens problem through
   ; backtracking, depth-first search
   (local (
-          (define opportunities (shuffle (availability board))))
+          (define opportunities
+            (sort (shuffle (availability board)) options<?)))
     (place-queen opportunities board)))
 
 
@@ -74,6 +75,64 @@
     [else (cons (first lst) (list->set (rest lst)))]))
 
 
+(define (shuffle lst)
+  ; [ListOf X] -> [ListOf X]
+  ; shuffle a list of X in the most efficien manner
+  (local (
+          (define-struct shufflor [rand ord orig])
+          (define ini-shf (make-shufflor '() '() lst))
+          (define (randomize shf)
+            ;Shufflor -> [ListOf X]
+            (local (
+                    (define rnd (shufflor-rand shf))
+                    (define ordr (shufflor-ord shf))
+                    (define og (shufflor-orig shf))
+                    (define (take n s)
+                      ; N Shufflor -> Shufflor
+                      (local (
+                              (define r (shufflor-rand s))
+                              (define d (shufflor-ord s))
+                              (define o (shufflor-orig s)))
+                        (cond
+                          [(= n 0) (randomize (make-shufflor
+                                               (cons (first o) r)
+                                               '() (append d (rest o))))]
+                          [else (take (sub1 n)
+                                      (make-shufflor
+                                       r (cons (first o) d) (rest o)))]))))
+              ; - IN -              
+              (cond
+                [(and (empty? ordr) (empty? og)) rnd]
+                [(or (empty? og) (empty? (rest og)))
+                 (randomize (make-shufflor (cons (first og) rnd) '() ordr))]
+                [else (take (random (length og)) shf)]))))
+    ; - IN -
+    (randomize ini-shf)))
+
+
+(define (construct-diagonals n op c0)
+  ; N [Number Number -> Number] N -> [ListOf [ListOf N]]
+  ; constructs the pos- and neg-sloped diagonals
+  (build-list
+   (- (* 2 n) 1)
+   (lambda (z)
+     (foldr append '()
+            (build-list
+             n
+             (lambda (y)
+               (filter (lambda (r) (>= r 0))
+                       (build-list
+                        n
+                        (lambda (x)
+                          (if (= (op x y) (- z c0)) (+ (* y n) x) -1))))))))))
+
+
+(define (display-square n)
+  ; N -> Img
+  ; generates a visually appealing image of a sudoku square
+  (overlay (circle (/ TEXTSIZE 3) "solid" (if (= n 0) "white" "black")) SQUARE))
+
+
 ;; !!! refactor this
 (define (display board)
   ; [ListOf X] -> Img
@@ -105,73 +164,17 @@
                               (map display-square r))) rows))))
 
 
-(define (shuffle lst)
-  ; [ListOf X] -> [ListOf X]
-  ; shuffle a list of X in the most efficien manner
-  (local (
-          (define-struct shufflor [rand ord orig])
-          (define ini-shf (make-shufflor '() '() lst))
-          (define (randomize shf)
-            ;Shufflor -> [ListOf X]
-            (local (
-                    (define rnd (shufflor-rand shf))
-                    (define ordr (shufflor-ord shf))
-                    (define og (shufflor-orig shf))
-                    (define (take n s)
-                      ; N Shufflor -> Shufflor
-                      (local (
-                              (define r (shufflor-rand s))
-                              (define d (shufflor-ord s))
-                              (define o (shufflor-orig s)))
-                        (cond
-                          [(= n 0) (randomize (make-shufflor
-                                               (cons (first o) r)
-                                               '() (append d (rest o))))]
-                          [else (take (sub1 n)
-                                      (make-shufflor
-                                       r (cons (first o) d) (rest o)))]))))
-              ; - IN -              
-              (cond
-                [(and (empty? ordr) (empty? og)) rnd]
-                [(< (length og) 2)
-                 (randomize (make-shufflor (cons (first og) rnd) '() ordr))]
-                [else (take (random (length og)) shf)]))))
-    ; - IN -
-    (randomize ini-shf)))
-
-
-(define (construct-diagonals n op c0)
-  ; N [Number Number -> Number] N -> [ListOf [ListOf N]]
-  ; constructs the pos- and neg-sloped diagonals
-  (build-list
-   (- (* 2 n) 1)
-   (lambda (z)
-     (foldr append '()
-            (build-list
-             n
-             (lambda (y)
-               (filter (lambda (r) (>= r 0))
-                       (build-list
-                        n
-                        (lambda (x)
-                          (if (= (op x y) (- z c0)) (+ (* y n) x) -1))))))))))
-
-
-(define (display-square n)
-  ; N -> Img
-  ; generates a visually appealing image of a sudoku square
-  (overlay (circle (/ TEXTSIZE 3) "solid" (if (= n 0) "white" "black")) SQUARE))
-
-
 
 ; =======================
 ; constants
 
-(define WIDTH 8)
+(define WIDTH 9)
 (define ROWS
-  (build-list WIDTH (lambda (y) (build-list WIDTH (lambda (x) (+ (* WIDTH y) x))))))
+  (build-list WIDTH
+              (lambda (y) (build-list WIDTH (lambda (x) (+ (* WIDTH y) x))))))
 (define COLUMNS
-  (build-list WIDTH (lambda (y) (build-list WIDTH (lambda (x) (+ y (* WIDTH x)))))))
+  (build-list WIDTH
+              (lambda (y) (build-list WIDTH (lambda (x) (+ y (* WIDTH x)))))))
 (define DIAGONALS+ (construct-diagonals WIDTH + 0))
 (define DIAGONALS- (construct-diagonals WIDTH - (- WIDTH 1)))
 (define ZONES (append ROWS COLUMNS DIAGONALS+ DIAGONALS-))
@@ -189,6 +192,12 @@
 (define BLANKTANGLE (rectangle 0 0 "solid" "white"))
 
 
+(define (options<? x y)
+  ; N N -> Boolean
+  ; #t if a queen at x threatens fewer squares than one at y, else #f
+  (< (length (@index x THREATS)) (length (@index y THREATS))))
+
+
 
 ; =======================
 ; checks
@@ -197,7 +206,7 @@
 (check-expect (construct-diagonals 3 + 0)  '((0) (1 3) (2 4 6) (5 7) (8)))
 (check-expect (construct-diagonals 3 - 2)  '((6) (3 7) (0 4 8) (1 5) (2)))
 (check-expect (availability EMPTYBOARD) ITERATOR)
-
+(check-expect (@index 3 '(5 4 3 2 1)) 2)
 
 
 ; ========================
