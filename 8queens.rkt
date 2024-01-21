9013a31ccd53655f604b14bc732fd150544b3bea
Robert Lutz <robertlutzi@gmail.com>
2024-01-20 21:38:12 -0500

    implement availability function

--- 8queens.rkt
+++ 8queens.rkt
@@ -5,7 +5,7 @@
 ; ==========================
 ; constants
 
-(define WIDTH 3)
+(define WIDTH 8)
 (define ROWS
   (build-list WIDTH (lambda (y) (build-list WIDTH (lambda (x) (+ (* WIDTH y) x))))))
 (define COLUMNS
@@ -29,11 +29,70 @@
 (define DIAGONALS- (construct-diagonals WIDTH - (- WIDTH 1)))
 (define ZONES (append ROWS COLUMNS DIAGONALS+ DIAGONALS-))
 (define ITERATOR (build-list (sqr WIDTH) identity))
+(define EMPTYBOARD (make-list (sqr WIDTH) 0))
+
+
+; =========================
+; functions
+#;
+(define (solve board)
+  ; [ListOf N] -> [ListOf N]
+  ; attempts to solve the NQueens problem through
+  ; backtracking, depth-first search
+  (local (
+          (define openings (availability board)))))
+
+
+(define (availability board)
+  ; [ListOf N] -> [ListOf Boolean]
+  ; derive the list of possible values where the next queen can go
+  (local (
+          (define (rastor n)
+            ; N [ListOf N] -> Boolean
+            ; determines if a queen can slot into a particular square
+            (cond
+              [(= (read-square n board) 1) #f]
+              [else
+               (local (
+                       (define exclusion-zone
+                         (list-to-set (foldr append '()
+                                             (filter (λ (z) (member? n z)) ZONES)))))
+                 ; - IN -
+                 (andmap (λ (sq) (= (read-square sq board) 0))
+                         exclusion-zone))])))
+    ; - IN -
+    (map rastor ITERATOR)))
+
+
+(define (read-square n board)
+  ; N [ListOf N] -> N
+  ; retrieves the value of the square at position n
+  (cond
+    [(= 0 n) (first board)]
+    [else (read-square (sub1 n) (rest board))]))
+
+
+(define (list-to-set lst)
+  ; [ListOf X] -> [ListOf X]
+  ; converts a list into a set
+  (cond
+    [(empty? lst) '()]
+    [(member? (first lst) (rest lst)) (list-to-set (rest lst))]
+    [else (cons (first lst) (list-to-set (rest lst)))]))
 
 
 
 ; =======================
 ; checks
 
+(define q1 (cons 1 (rest EMPTYBOARD)))
 (check-expect (construct-diagonals 3 + 0)  '((0) (1 3) (2 4 6) (5 7) (8)))
 (check-expect (construct-diagonals 3 - 2)  '((6) (3 7) (0 4 8) (1 5) (2)))
+(check-expect (availability q1) (list #f #f #f #f #f #f #f #f
+                                      #f #f #t #t #t #t #t #t
+                                      #f #t #f #t #t #t #t #t
+                                      #f #t #t #f #t #t #t #t
+                                      #f #t #t #t #f #t #t #t
+                                      #f #t #t #t #t #f #t #t
+                                      #f #t #t #t #t #t #f #t
+                                      #f #t #t #t #t #t #t #f))
